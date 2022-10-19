02c0a99b5a86739438279846485cd5ded4bc1dea
naedong <hww23@naver.com>
2022-10-19 21:50:23 +0900

    연습용 2D

---
+++ Assets/testS.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+// 게임 오브젝트를 계속 왼쪽으로 움직이는 스크립트
+public class testS : MonoBehaviour {
+    public float speed = 10f; // 이동 속도
+
+     private void Update() {
+        // 게임 오브젝트를 왼쪽으로 일정 속도로 평행 이동하는 처리
+        if( !GameManager.instance.isGameover){
+        transform.Translate(Vector3.left * speed * Time.deltaTime);
+        }
+    }
+
+    void Start()
+    {
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = 10;
+    }
+}
